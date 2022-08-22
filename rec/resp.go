9bfc60aa7103433c83b8e7a5d74fda98e2f09e0d
piupuer <piupuer@gmail.com>
2022-08-22 17:39:46 +0800

    split points

--- rec/resp.go
+++ rec/resp.go
@@ -3,7 +3,10 @@ package rec
 import "github.com/shopspring/decimal"
 
 type Res struct {
-	Text   string                `json:"text"`
-	Acc    decimal.Decimal       `json:"acc"`
-	Points [4][2]decimal.Decimal `json:"points"`
+	Text string             `json:"text"`
+	Acc  decimal.Decimal    `json:"acc"`
+	P0   [2]decimal.Decimal `json:"p0"` // left-top point
+	P1   [2]decimal.Decimal `json:"p1"` // right-top point
+	P2   [2]decimal.Decimal `json:"p2"` // right-bottom point
+	P3   [2]decimal.Decimal `json:"p3"` // left-bottom point
 }
